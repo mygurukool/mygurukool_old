edad4abf2b0a8933da22ae459490e494ed652b94
Rajesh Goud Gudidevuni <rajesh.gudidevuni@valuelabs.com>
2020-05-07 20:45:10 +0530

    MyGurukool Changes

--- src/components/Student.js
+++ src/components/Student.js
@@ -17,11 +17,27 @@ import {
  
 // Demo styles, see 'Styles' section below for some notes on use.
 import 'react-accessible-accordion/dist/fancy-example.css';
+import stripHtml from "string-strip-html";
+// import parse from 'html-react-parser';
 const override = css`
   display: block;
   margin: 0 auto;
   border-color: red;
 `;
+
+
+const cb = ({
+  tag,
+  deleteFrom,
+  deleteTo,
+  insert,
+  rangesArr,
+  proposedReturn,
+}) => {
+  // default action which does nothing different from normal, non-callback operation
+  rangesArr.push(deleteFrom, deleteTo, insert);
+  // you might want to do something different, depending on "tag" contents.
+};
 export default class Student extends Component {
 	constructor(props) {
 	    super(props);
@@ -35,12 +51,13 @@ export default class Student extends Component {
 	      exercisedata:'',
 	      isLoading: false,
 	    };
+	    this.handleSubmitClick = this.handleSubmitClick.bind(this)
 	  
 	}
 	componentDidMount() {
 	 	this.state.isLoading = true;
 
-	 	localStorage.setItem('token','eyJ0eXAiOiJKV1QiLCJub25jZSI6Ikd1ZTZrVFpWeFVDaVVzRjZMeW5Xel9FYVo5TEI0dFNwaXVKRlRYMkx1bjQiLCJhbGciOiJSUzI1NiIsIng1dCI6IkN0VHVoTUptRDVNN0RMZHpEMnYyeDNRS1NSWSIsImtpZCI6IkN0VHVoTUptRDVNN0RMZHpEMnYyeDNRS1NSWSJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8xNzVjZjE0YS0xOGRhLTQwNGYtOTcwMy04NjNlOGQ4Y2FlNDcvIiwiaWF0IjoxNTg4NTg0NjcyLCJuYmYiOjE1ODg1ODQ2NzIsImV4cCI6MTU4ODU4ODU3MiwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IkFVUUF1LzhQQUFBQTFPOU9lK0hhb3RpMmxGWnR3dnc4Tk9NOEhqTmZnRy9BdVNOQXFNOGQ3cWRtcFJzSG1XQk1GMlVUdHpOUEtaM3BGRVJuNU56cTVIdnVSdVAyRXZxV2JRPT0iLCJhbXIiOlsicHdkIiwibWZhIl0sImFwcF9kaXNwbGF5bmFtZSI6Im15T25lbm90ZUFjY2Vzc0FwcCIsImFwcGlkIjoiMWY0ZGRlZmMtZDg0OS00ZjMwLWE0MTAtNzMyOTdlZDk4NDIyIiwiYXBwaWRhY3IiOiIwIiwiaXBhZGRyIjoiMjcuNTkuMjQzLjIxOSIsIm5hbWUiOiJNeUd1cnVLb29sIERldmVsb3BlciIsIm9pZCI6IjU1MGU0MjJiLThmZmUtNGMzMS1hMGY1LTY4YzRjODQzMmYyNCIsInBsYXRmIjoiMyIsInB1aWQiOiIxMDAzMjAwMEI5RjFEQ0UyIiwic2NwIjoiTm90ZXMuUmVhZC5BbGwgcHJvZmlsZSBvcGVuaWQgZW1haWwiLCJzaWduaW5fc3RhdGUiOlsia21zaSJdLCJzdWIiOiJYaHBWa0pfQjZ0QkhfWkZmMGZya3lmQVNVMzBzRXJCRFRMRU0tMU8wREcwIiwidGlkIjoiMTc1Y2YxNGEtMThkYS00MDRmLTk3MDMtODYzZThkOGNhZTQ3IiwidW5pcXVlX25hbWUiOiJkZXZAbXlndXJ1a29vbC5vbm1pY3Jvc29mdC5jb20iLCJ1cG4iOiJkZXZAbXlndXJ1a29vbC5vbm1pY3Jvc29mdC5jb20iLCJ1dGkiOiJabUtRZmsyTFhFNmNmSUxIdUFBZEFRIiwidmVyIjoiMS4wIiwieG1zX3N0Ijp7InN1YiI6InpoaEtya2NFVWJQdHVnVWlOSTFoSFprSW9UMFJWbV8wT3BueS1xUDNxV3MifSwieG1zX3RjZHQiOjE1ODc4Mjg0ODJ9.qhKN7OBxrwc6eL--OnzBo-_oyDXwOPAaaZMN2hc5tCglHHv9Nxduhc2Dc_isMpFHode8D30SeYVmOtPSXUA--BWqq51ze2Ee4BPC3XEPbRavT_1kQybgA7FJqTHbmc_gwUL2r9G9iYhG2E1qRAtJP_orTxKvUJiqXm__PiEQx2OapHBzSC9eFAuCvbsKn75_WLqwxddGWZ0vnq6YotcRJb4cFdM5UZ96IkQbozZEehlEGajrPQv-WnjtiDJCD7_XpqI5fPPyBCaOOwNjh5cCZvWHhFVnFjB5PaOyOzQbL_wYJIFUUA0VVNCfJipD780uVUcnDWWME_dfr_9jq1bVfQ');
+	 	localStorage.setItem('token','eyJ0eXAiOiJKV1QiLCJub25jZSI6ImpqN3pDQkdKVXZGLUhueFlhMWVZZkp6TEcyNlNjZHhSNUFYZHJuRjZGSnMiLCJhbGciOiJSUzI1NiIsIng1dCI6IkN0VHVoTUptRDVNN0RMZHpEMnYyeDNRS1NSWSIsImtpZCI6IkN0VHVoTUptRDVNN0RMZHpEMnYyeDNRS1NSWSJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8xNzVjZjE0YS0xOGRhLTQwNGYtOTcwMy04NjNlOGQ4Y2FlNDcvIiwiaWF0IjoxNTg4ODU3NjUyLCJuYmYiOjE1ODg4NTc2NTIsImV4cCI6MTU4ODg2MTU1MiwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IkFVUUF1LzhQQUFBQWdnWkJibk1wOXpuK1BtY29VaDJ1Rm42eGZ5cVk2NDNpMmVCbWFKcmgvOHJ4M0wrRCthZ04vZ2RKUmdzbjIrODN2QnZ4aG1vaXF3eDRQOTRvVlBHVlZBPT0iLCJhbXIiOlsicHdkIiwibWZhIl0sImFwcF9kaXNwbGF5bmFtZSI6Im15T25lbm90ZUFjY2Vzc0FwcCIsImFwcGlkIjoiMWY0ZGRlZmMtZDg0OS00ZjMwLWE0MTAtNzMyOTdlZDk4NDIyIiwiYXBwaWRhY3IiOiIwIiwiaXBhZGRyIjoiMTgzLjgzLjY3LjE5NyIsIm5hbWUiOiJNeUd1cnVLb29sIERldmVsb3BlciIsIm9pZCI6IjU1MGU0MjJiLThmZmUtNGMzMS1hMGY1LTY4YzRjODQzMmYyNCIsInBsYXRmIjoiMyIsInB1aWQiOiIxMDAzMjAwMEI5RjFEQ0UyIiwic2NwIjoiTm90ZXMuUmVhZC5BbGwgcHJvZmlsZSBvcGVuaWQgZW1haWwiLCJzaWduaW5fc3RhdGUiOlsia21zaSJdLCJzdWIiOiJYaHBWa0pfQjZ0QkhfWkZmMGZya3lmQVNVMzBzRXJCRFRMRU0tMU8wREcwIiwidGlkIjoiMTc1Y2YxNGEtMThkYS00MDRmLTk3MDMtODYzZThkOGNhZTQ3IiwidW5pcXVlX25hbWUiOiJkZXZAbXlndXJ1a29vbC5vbm1pY3Jvc29mdC5jb20iLCJ1cG4iOiJkZXZAbXlndXJ1a29vbC5vbm1pY3Jvc29mdC5jb20iLCJ1dGkiOiItT211OFVpOUsweUFPa2pyVGRFYkFBIiwidmVyIjoiMS4wIiwieG1zX3N0Ijp7InN1YiI6InpoaEtya2NFVWJQdHVnVWlOSTFoSFprSW9UMFJWbV8wT3BueS1xUDNxV3MifSwieG1zX3RjZHQiOjE1ODc4Mjg0ODJ9.LUT0Ye5tBSqJbKVatMcVUZMGb-AsUTpVnrW1ARgc4_-1IBfgGPrDYENPEoMW8bMI9zjsVxP4ZbMJ0cdnKQhc5y7LkUhlhKCkKH_mz1skpEjTTahvtBbfxx7ez02kZeEZjTIbc4fwEZWYGpQ92D5p5zJ6pn2xNv2Xzbj15P8STFCOkQCg4LMRbcdSFkZu70WLfBLei8cOEdaN-q1yjbCx16nevZ_q98SVfrrgfEpA3Bw2_J1Zpes7nappJjMch-9NN1okVwW4WasiyyaGq774uM5aOrFYlEvX7bqU4-0Hl82UDfubOGtd5J8K6JZ0obD3fN798H5dKdWneGvRgOAUPw');
 	 	// axios.defaults.headers.common['Authorization'] = "Bearer " +localStorage.getItem('userDetails');
 	 	const config = {
 		    headers: { Authorization: `Bearer ${localStorage.getItem('userDetails')}` }
@@ -74,6 +91,11 @@ export default class Student extends Component {
 	    	})
     	});
     }
+
+    handleSubmitClick = (event) => {
+    	alert("ellpo")
+    }
+
     handleClick = (event) => {
     	this.state.isLoading = true;
     	// alert(event.target.id);
@@ -89,14 +111,35 @@ export default class Student extends Component {
 					axios.get(exe.contentUrl,
 						{ params:{}, headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` } }
 			    	).then( response => {
-							this.state.exercise.value[i].content = response.data;
-							this.setState({ exercisedata: this.state.exercise });
+			    			axios.post("http://training.localhost/mygurukool.php",
+					  		{
+							    data: response.data,
+							    // password: this.state.password,
+							}).then(res => {
+					  			this.state.exercise.value[i].content = res.data;
+					  			console.log(res)
+					  			this.setState({ exercisedata: this.state.exercise });
+					  		})
+					// 		this.state.exercise.value[i].content = response.data;
+
+					// 		
+
+					// 		// console.log(stripHtml(this.state.exercise.value[i].content));
+					// 		// parse(this.state.exercise.value[i].content, {
+					// 		//   replace: function(domNode) {
+					// 		//     // console.log(domNode, { depth: null });
+					// 		//     c
+					// 		//   }
+					// 		// });
+					// 		console.log(stripHtml(this.state.exercise.value[i].content, {cb} ));
 			    	})
+			  		
 				)
 			}
     	})
     	return false;
 	}
+	
 	render() {
 		return	(
 			<Fragment>
@@ -142,6 +185,7 @@ export default class Student extends Component {
 				          color={"#D77F36"}
 				          loading={this.state.isLoading}
 				        />
+				        <i class="fas fa-download"></i>
 				        <Accordion>
 				 			{ 
 				 				this.state.exercisedata && this.state.exercisedata.value.map((exe,i) =>
@@ -152,9 +196,9 @@ export default class Student extends Component {
 						                        	exe.title ? exe.title : 'No Exercise Data'
 						                        }  
 						                    </AccordionItemButton>
-						                </AccordionItemHeading>
+						                </AccordionItemHeading >
 						                <AccordionItemPanel dangerouslySetInnerHTML={{ __html: exe.content }}>
-						                    
+
 						                </AccordionItemPanel>
 				 					</AccordionItem>
 				 				)
